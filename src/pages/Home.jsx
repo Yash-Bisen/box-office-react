0262f5848bafb1c75fb358022d55897a9b63127c
yash Bisen <yash0803@gmail.com>
2023-06-11 18:56:50 +0530

    added searchbox and button click4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    return <div> Home </div>
-}
+    const [SearchStr, setSearchStr] = useState("");
+
+    const onSearchInputChange = (ev) =>{
+        setSearchStr(ev.target.value);
+    };
+
+    const onSearch =async(ev)=>{
+        ev.preventDefault();
+
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${SearchStr}`)
+        const body = await response.json();
+        
+        console.log(body)
+        //https://api.tvmaze.com/search/shows?q=boys
+    };
+    
+    return (
+    <div> 
+
+        <form onSubmit={onSearch}>
+        <input type="text" value={SearchStr} onChange={onSearchInputChange}/>
+        <button
+            type="button"
+            onClick={()=>{
+                setInputValue("And");
+            }}
+        >Search
+        </button>
+       </form>
+         </div>
+    );
+};
 
 export default Home;
